0f3c4456a02a79980a7b8f92a2355efdfcf17791
Michael D. Norman <mdnorman@users.noreply.github.com>
2024-02-01 17:00:06 -0600

    feat: added linux build

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-cd src
-go build -o ../bin/gomove ./main
+SRC_DIR=./src
+OUT_DIR=../bin
+
+cd $SRC_DIR
+
+mkdir -p $OUT_DIR/macos
+GOOS=darwin GOARCH=amd64 go build -o $OUT_DIR/macos/gomove ./main
+
+mkdir -p $OUT_DIR/linux
+GOOS=linux GOARCH=amd64 go build -o $OUT_DIR/linux/gomove ./main
